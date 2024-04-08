09420bc1aa27e377ccd3746feb1291480929cc93
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-04-08 15:03:21 +0530

    Update predict.py

--- API/routes/predict.py
+++ API/routes/predict.py
@@ -6,3 +6,18 @@ router = APIRouter(
     responses={404: {"description": "The requested URI was not found"}},
 )
 
+
+@router.post("/")
+async def predict_image_class(
+        image: UploadFile = File(...)
+):
+    """
+    The endpoint for predicting the class of an image
+
+    Args:
+        image_data (UploadFile): the image to predict
+
+    Returns:
+        (str) The class of the image
+    """
+    return {"class": "Class", "confidence": 0.0}
