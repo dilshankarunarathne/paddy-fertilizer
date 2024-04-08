bd12dac3e5108a49fe858738de85b5d9be2571aa
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-04-08 16:01:10 +0530

    enhancement: CORS

--- API/main.py
+++ API/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from routes import predict
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(predict.router)
