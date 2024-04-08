08b81380a7a183294d6d6d81cce25bb92711188d
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-04-08 15:19:54 +0530

    enhancement: npa estimation

--- API/routes/predict.py
+++ API/routes/predict.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, UploadFile, File
 from PIL import Image
 import numpy as np
 from io import BytesIO
-from lcc.main import predict_image_class
+from lcc.main import predict_image_class, estimate_npa
 
 router = APIRouter(
     prefix="/api/predict",
@@ -34,7 +34,13 @@ async def predict_image(image: UploadFile = File(...)):
     # Call the predict_image_class method with the numpy array
     class_name, confidence_score = predict_image_class(image_array)
 
+    # Remove trailing newline character from class_name
+    class_name = class_name.strip()
+
     # Convert numpy.float32 to float
     confidence_score = float(confidence_score)
 
-    return {"class_name": class_name, "confidence_score": confidence_score}
+    # Estimate npa
+    npa_estimate = estimate_npa(class_name)
+
+    return {"class_name": class_name, "confidence_score": confidence_score, "npa_estimate": npa_estimate}
