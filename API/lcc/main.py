c0ebedbaffaf8c8376ab187989c0670838cf82b2
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-04-08 15:07:15 +0530

    Update main.py

--- API/lcc/main.py
+++ API/lcc/main.py
@@ -7,7 +7,6 @@ def predict_image_class(image_data):
 
     model = load_model("lcc/keras_Model.h5", compile=False)
 
-    # Load the labels
     class_names = open("labels.txt", "r").readlines()
 
     # Create the array of the right shape to feed into the keras model
